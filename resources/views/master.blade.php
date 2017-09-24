cbb7207f9c85a3ffcf9b349711cb6c95f21163ff
DipayanSenGupta <dip.himu@gmail.com>
2017-09-25 03:34:52 +0600

    Crud Application

---
+++ resources/views/master.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="X-UA-Compatible" content="TE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CRUD Operations</title>
+
+    <link rel="stylesheet" href="{{asset('css/app.css')}}" rel="stylesheet" type="text/css">
+
+  </head>
+  <body>
+    <br><br>
+    @yield('content');
+  </body>
+</html>
